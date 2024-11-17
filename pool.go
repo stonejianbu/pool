7f94ea63979888d5d7d141c6508bc9f30ead511d
huangyouguang <huangyouguang@ideal.sh.cn>
2024-11-17 21:51:06 +0800

    feat: 一次性的单一协程池实现

--- pool.go
+++ pool.go
@@ -6,47 +6,18 @@ import (
 	"sync"
 )
 
-type MsgType int
-
-const (
-	DefaultMsgType MsgType = iota
-	AMsgType
-	BMsgType
-	CMsgType
-	DMsgType
-	EMsgType
-	FMsgType
-	GMsgType
-)
-
-type Msg struct {
-	Type    MsgType
-	Content interface{}
-}
-
-type RespMsg struct {
-	Type    MsgType
-	Error   error
-	Content interface{}
-}
-
-func NewMsgList(msgType MsgType, items []interface{}) []Msg {
-	msgList := make([]Msg, 0, len(items))
-	for _, item := range items {
-		msgList = append(msgList, Msg{Type: msgType, Content: item})
-	}
-	return msgList
-}
-
 type Pool struct {
-	handlerFuncMap map[MsgType]func(context.Context, Msg) *RespMsg
-	msgChan        chan Msg
-	msgWg          sync.WaitGroup
-	num            uint
-	resultChan     chan *RespMsg
-	resultList     []*RespMsg
-	mux            sync.Mutex
-	resultWg       sync.WaitGroup
+	handlerFunc  func(ctx context.Context, msg interface{}) (interface{}, error)
+	justDoItFunc func(ctx context.Context, msg interface{})
+	msgChan      chan interface{}
+	msgWg        sync.WaitGroup
+	num          uint
+	resultChan   chan interface{}
+	resultList   []interface{}
+	resultWg     sync.WaitGroup
+	err          error
+	errMux       sync.RWMutex
+	ignoreErr    bool
 }
 
 // NewPool 创建一个新的任务处理池。
@@ -55,13 +26,13 @@ type Pool struct {
 func NewPool(ctx context.Context, num uint) *Pool {
 	// 初始化Pool结构体实例。
 	instance := &Pool{
-		msgChan:        make(chan Msg, 2*num),                             // 创建一个固定容量的消息通道。
-		msgWg:          sync.WaitGroup{},                                  // 初始化用于同步消息处理的WaitGroup。
-		num:            num,                                               // 设置工作器数量。
-		handlerFuncMap: map[MsgType]func(context.Context, Msg) *RespMsg{}, // 初始化消息处理函数映射。
-		resultChan:     make(chan *RespMsg, 2*num),                        // 创建一个固定容量的结果通道。
-		resultList:     make([]*RespMsg, 0),                               // 初始化存储结果的切片。
-		resultWg:       sync.WaitGroup{},                                  // 初始化用于同步结果处理的WaitGroup。
+		msgChan:    make(chan interface{}, 2*num), // 创建一个固定容量的消息通道。
+		msgWg:      sync.WaitGroup{},              // 初始化用于同步消息处理的WaitGroup。
+		num:        num,                           // 设置工作器数量。
+		resultChan: make(chan interface{}, 2*num), // 创建一个固定容量的结果通道。
+		resultList: make([]interface{}, 0),        // 初始化存储结果的切片。
+		resultWg:   sync.WaitGroup{},              // 初始化用于同步结果处理的WaitGroup。
+		errMux:     sync.RWMutex{},
 	}
 	// 启动消息消费者协程，负责从消息通道中消费消息并进行处理。
 	instance.consumer(ctx)
@@ -74,13 +45,16 @@ func NewPool(ctx context.Context, num uint) *Pool {
 // Producer 向消息池中发送消息。
 // 该方法接收一个可变数量的Msg参数，并将它们逐个发送到消息通道中。
 // 参数：
-//   msgs ...Msg: 要发送到消息池中的消息列表。
+//   msgs ...interface{}: 要发送到消息池中的消息列表。
 // 注意：
 //   1. 该方法通过循环遍历传入的消息列表，并使用channel发送操作（<-）将每个消息发送到消息通道中。
 //   2. 调用该方法前，需要确保消息池已经被正确初始化并且消息通道（msgChan）是可写入的。
-func (that *Pool) Producer(msgs ...Msg) {
+func (that *Pool) Producer(msgs ...interface{}) {
 	// 遍历传入的所有消息，并将它们逐个发送到消息通道中。
 	for _, msg := range msgs {
+		if that.getError() != nil {
+			return
+		}
 		that.msgChan <- msg
 	}
 }
@@ -91,28 +65,17 @@ func (that *Pool) Producer(msgs ...Msg) {
 // 接着，关闭结果通道（resultChan），确保所有结果都被取出。
 // 最后，等待与结果处理相关的等待群（resultWg）完成。
 // 返回值是解析后的响应消息列表。
-func (that *Pool) Wait() []*RespMsg {
+func (that *Pool) Wait() ([]interface{}, error) {
 	// 关闭消息通道，确保所有消息发送完毕
 	close(that.msgChan)
-	// 等待所有消息被处理
+	// 等待所有消息被处理完成
 	that.msgWg.Wait()
-	// 关闭结果通道，确保所有结果都被接收
+	// 所有消息被处理完成时，也同时表明所有结果已进入保存结果的队列，关闭结果通道
 	close(that.resultChan)
 	// 等待所有结果处理完成
 	that.resultWg.Wait()
 	// 返回解析后的结果列表
-	return that.resultList
-}
-
-// RegisterHandlerFunc 为特定消息类型注册处理函数。
-//
-// msgType: 指定需要注册处理函数的消息类型。
-// handler: 处理函数，接收一个context和一个消息，返回一个响应消息。
-//
-// 该方法允许消息池为特定的消息类型关联一个处理函数，当接收到已注册消息类型的消息时，
-// 消息池会调用对应的处理函数来处理消息。
-func (that *Pool) RegisterHandlerFunc(msgType MsgType, handler func(context.Context, Msg) *RespMsg) {
-	that.handlerFuncMap[msgType] = handler
+	return that.resultList, that.err
 }
 
 // getResult 从 resultChan 中接收结果消息，并将其添加到 resultList 中
@@ -140,81 +103,86 @@ func (that *Pool) consumer(ctx context.Context) {
 		that.msgWg.Add(1)
 		go func() {
 			defer that.msgWg.Done()
-			for msg := range that.msgChan {
-				if handler := that.handlerFuncMap[msg.Type]; handler != nil {
-					that.resultChan <- handler(ctx, msg)
-				} else {
-					panic(fmt.Sprintf("unknown msgType: %d", msg.Type))
+			defer func() {
+				if err := recover(); err != nil {
+					that.setError(fmt.Errorf("panic recover err: %v", err))
+					return
 				}
+			}()
+			for msg := range that.msgChan {
+				select {
+				case <-ctx.Done(): // 超时判断
+					// 超时设置处理异常，其它协程捕获到异常时会退出
+					that.setError(ctx.Err())
+					return
+				default:
+					if that.ignoreErr {
+						// 只是执行处理（除panic异常外，不捕获异常）
+						that.justDoItFunc(ctx, msg)
+					} else {
+						// 如果捕获到处理异常则退出
+						if that.getError() != nil {
+							return
+						}
+						// 执行处理逻辑
+						resp, err := that.handlerFunc(ctx, msg)
+						if err != nil {
+							that.setError(err)
+							return
+						}
+						that.resultChan <- resp
+					}
+				}
+
 			}
 		}()
 	}
 }
 
-// DefaultProducer 是一个方法，用于创建并发送默认类型的MsgList。
-// 该方法属于Pool结构。
-// 参数 params 用于初始化MsgList的内容。
-// 该方法通过调用Pool的Producer方法，将创建的MsgList作为参数传递。
-//
-// 该方法的主要目的是简化消息生产的过程，
-// 通过使用默认的消息类型和提供的参数来创建消息列表（MsgList），
-// 并触发这些消息的处理。
-func (that *Pool) DefaultProducer(params ...interface{}) {
-	that.Producer(NewMsgList(DefaultMsgType, params)...)
+// getError 获取处理错误
+func (that *Pool) getError() error {
+	that.errMux.RLock()
+	defer that.errMux.RUnlock()
+	return that.err
 }
 
-// RegisterDefaultHandlerFunc registers a default message handler.
-//
-// This function allows the user to specify a handler for default messages, enhancing the flexibility and usability of the message processing system.
-// Parameters:
-// - handler: A function that processes the default message. It accepts a context and a message object, and returns a response message object.
-//
-// Note: This method does not return any value, but modifies the state of the current Pool instance by registering the handler function under the default message type.
-func (that *Pool) RegisterDefaultHandlerFunc(handler func(context.Context, Msg) *RespMsg) {
-	that.RegisterHandlerFunc(DefaultMsgType, handler)
+// setError 赋值处理错误
+func (that *Pool) setError(err error) {
+	that.errMux.Lock()
+	defer that.errMux.Unlock()
+	that.err = err
+	return
 }
 
 // Do 执行特定任务的函数，使用任务池进行管理并行处理
-// 该函数接收一个上下文、一个无符号整数、一个处理器函数和一组可变参数
-// 返回一个响应消息切片
-func Do(ctx context.Context, num uint, handler func(context.Context, Msg) *RespMsg, params ...interface{}) []*RespMsg {
+// 该函数接收一个上下文context、一个无符号整数、一个处理器函数和一组可变参数
+// 当处理过程出现异常会继续执行处理，直至任务处理完成
+func Do(ctx context.Context, num uint, handler func(context.Context, interface{}), params ...interface{}) {
+	// 创建并初始化一个任务池
+	p := NewPool(ctx, num)
+	// 忽视处理错误，不返回结果列表，just do it
+	p.ignoreErr = true
+	// 注册默认的处理器函数到任务池
+	p.justDoItFunc = handler
+	// 将参数加入到默认的生产者中，触发任务的生成
+	p.Producer(params...)
+	// 关闭消息通道，确保所有消息发送完毕
+	close(p.msgChan)
+	// 等待所有消息被处理完成
+	p.msgWg.Wait()
+}
+
+// Go 执行特定任务的函数，使用任务池进行管理并行处理
+// 该函数接收一个上下文context、一个无符号整数、一个处理器函数和一组可变参数
+// 当处理过程出现异常则停止处理并返回错误
+// 返回一个响应消息切片和错误信息
+func Go(ctx context.Context, num uint, handler func(context.Context, interface{}) (interface{}, error), params ...interface{}) ([]interface{}, error) {
 	// 创建并初始化一个任务池
 	p := NewPool(ctx, num)
 	// 注册默认的处理器函数到任务池
-	p.RegisterDefaultHandlerFunc(handler)
+	p.handlerFunc = handler
 	// 将参数加入到默认的生产者中，触发任务的生成
-	p.DefaultProducer(params...)
+	p.Producer(params...)
 	// 等待所有任务完成，并返回结果切片
 	return p.Wait()
-}
-
-// FilterResults 根据指定消息类型筛选响应消息列表。
-// 该函数遍历响应消息列表，寻找与指定消息类型相匹配的消息，并收集这些消息的内容。
-// 如果某条匹配的消息带有错误信息，则立即返回错误并附带之前收集的所有内容。
-// 参数:
-//   t - 要筛选的消息类型。
-//   respMsgList - 响应消息列表，包含不同类型的消息。
-// 返回值:
-//   []interface{} - 收集到的消息内容，具体类型根据实际情况而定。
-//   error - 如果有消息携带错误信息，则返回该错误，否则返回nil。
-func FilterResults(t MsgType, respMsgList []*RespMsg) ([]interface{}, error) {
-	// 初始化收集到的消息内容列表。
-	rets := make([]interface{}, 0)
-	// 初始化错误变量，用于记录遇到的第一个错误。
-	var err error
-	// 遍历响应消息列表。
-	for _, msg := range respMsgList {
-		// 检查消息类型是否与目标类型匹配。
-		if msg.Type == t {
-			// 检查消息是否带有错误信息，如果有，则记录错误并提前返回。
-			if msg.Error != nil {
-				err = msg.Error
-				return rets, err
-			}
-			// 将匹配消息的内容添加到结果列表中。
-			rets = append(rets, msg.Content)
-		}
-	}
-	// 如果没有遇到错误，返回收集到的消息内容列表和nil错误。
-	return rets, nil
 }
--- pool_test.go
+++ pool_test.go
@@ -4,66 +4,66 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 )
 
-func TestNewPool(t *testing.T) {
-	ctx := context.Background()
-	p := NewPool(ctx, 5)
-	handler := func(ctx context.Context, msg Msg) *RespMsg {
-		fmt.Printf("msg: %+v\n", msg)
-		return &RespMsg{
-			Content: msg.Content,
-			Type:    msg.Type,
-		}
-	}
-	p.RegisterHandlerFunc(DefaultMsgType, handler)
-	p.RegisterHandlerFunc(AMsgType, handler)
-	p.RegisterHandlerFunc(BMsgType, handler)
-	msgList := NewMsgList(DefaultMsgType, []interface{}{"hello", "world", "pool"})
-	msgList = append(msgList, NewMsgList(AMsgType, []interface{}{1, 2, 3, 4, 5})...)
-	msgList = append(msgList, NewMsgList(BMsgType, []interface{}{"1", "2", "3", "4", "5"})...)
-	p.Producer(msgList...)
-	results := p.Wait()
-	for _, result := range results {
-		fmt.Printf("result: %+v\n", result)
-	}
-	if len(results) != len(msgList) {
-		t.Errorf("results length is not equal to params length")
-	}
-	r1, err := FilterResults(DefaultMsgType, results)
-	if err != nil {
-		t.Errorf("filter results error: %v", err)
-	}
-	if len(r1) != 3 {
-		t.Errorf("filter results length is not equal to params length")
-	}
-}
-
-func TestNewPool_Default(t *testing.T) {
-	ctx := context.Background()
-	p := NewPool(ctx, 10)
-	p.RegisterDefaultHandlerFunc(func(ctx context.Context, msg Msg) *RespMsg {
-		fmt.Printf("default msg: %+v\n", msg)
-		return &RespMsg{
-			Content: msg.Content,
-			Type:    msg.Type,
-		}
-	})
-	params := []interface{}{"hello", "world"}
-	p.DefaultProducer(params...)
-	results := p.Wait()
-	for _, result := range results {
-		fmt.Printf("result: %+v\n", result)
-	}
-	if len(results) != len(params) {
-		t.Errorf("results length is not equal to params length")
-	}
-
-}
-
 func TestDo(t *testing.T) {
-	Do(context.Background(), 5, func(ctx context.Context, msg Msg) *RespMsg {
+	// 假如有一万任务待执行，每个任务耗时0.5s
+	// 如果使用循环处理顺序执行，那耗时肯定很长
+	// 如果是一个任务启用一个协程处理，有一万个就启用一万个，可能程序会崩溃掉
+	// 为了避免程序崩溃，又让程序快点执行完成，我们需要限定开启协程的数量
+	taskParams := make([]interface{}, 10000)
+	startTime := time.Now()
+	ctx := context.Background()
+	Do(ctx, 500, func(ctx context.Context, msg interface{}) {
 		fmt.Printf("msg:%+v, do something\n", msg)
-		return nil
-	}, []interface{}{"1", "2", "3", "4", "5", "6", "7"}...)
+		// 这里可能执行调用第三方接口或数据库读写操作
+		time.Sleep(time.Millisecond * 500)
+		return
+	}, taskParams...)
+	fmt.Printf("耗时：%0.2f\n", time.Since(startTime).Seconds())
+}
+
+func TestGo(t *testing.T) {
+	taskParams := []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	startTime := time.Now()
+	ctx := context.Background()
+	rets, err := Go(ctx, 3, func(ctx context.Context, msg interface{}) (interface{}, error) {
+		fmt.Printf("msg:%+v, do something\n", msg)
+		// 这里可能执行调用第三方接口或数据库读写操作
+		time.Sleep(time.Second * 1)
+		m := msg.(int)
+		if m == 10 {
+			return nil, fmt.Errorf("msg信息异常")
+		}
+		return msg, nil
+	}, taskParams...)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, ret := range rets {
+		fmt.Printf("ret: %v\n", ret)
+	}
+	fmt.Printf("耗时：%0.2f\n", time.Since(startTime).Seconds())
+}
+
+func TestGo_WithTimeout(t *testing.T) {
+	taskParams := []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	startTime := time.Now()
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*3)
+	defer cancel()
+	rets, err := Go(ctx, 3, func(ctx context.Context, msg interface{}) (interface{}, error) {
+		fmt.Printf("msg:%+v, do something\n", msg)
+		// 这里可能执行调用第三方接口或数据库读写操作
+		time.Sleep(time.Second * 2)
+		return msg, nil
+	}, taskParams...)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, ret := range rets {
+		fmt.Printf("ret: %v\n", ret)
+	}
+	fmt.Printf("耗时：%0.2f\n", time.Since(startTime).Seconds())
 }
